505463a0c61a8ba833c8a32688412c3b75643db8
EJ Serafin <edgardo@chainblx.com>
2019-05-16 11:21:10 -0700

    added layout
    layout for constructors and methods added in

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,35 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+ class Game {
+     constructor(){
+         this.missed = 0;
+         this.phrases = phrases;
+         this.activePhrase = null;
+     }
+
+     startGame(){
+
+     }
+
+     getRandomPhrase(){
+
+     }
+
+     handleInteraction(){
+
+     }
+
+     removeLife(){
+         
+     }
+
+     checkForWin(){
+
+     }
+
+     gameOver(){
+         
+     }
+ }
