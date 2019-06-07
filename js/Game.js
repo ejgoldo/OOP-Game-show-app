61aab643eb68ba58a9ea5f6bd5837e0cad3ffdd7
EJ Serafin <edgardo@chainblx.com>
2019-06-07 13:33:06 -0700

    Phrase array added to game class

--- js/Game.js
+++ js/Game.js
@@ -5,8 +5,23 @@
  class Game {
      constructor(){
          this.missed = 0;
-         this.phrases = [];
+         this.phrases = this.createPhrases();
          this.activePhrase = null;
+     }
+
+
+     /**
+    * Creates phrases for use in game
+    * @return {array} An array of phrases that could be used in the game
+    */
+     createPhrases(){
+        const Phrase = [
+            "close but no cigar",
+            "a bird in the hand is worth two in the bush",
+            "batten down the hatches",
+            "early to bed and early to rise makes a man healthy, wealthy and wise",
+            "rag and bone man"
+        ]
      }
 
      startGame(){
